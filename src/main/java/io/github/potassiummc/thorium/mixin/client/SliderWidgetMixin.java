e49b69919ac25141f3a6a1be983eac6fb68f05c5
Noah van der Aa <ndvdaa@gmail.com>
2022-05-14 22:48:50 +0200

    Fix MC-145748

---
+++ src/main/java/io/github/potassiummc/thorium/mixin/client/SliderWidgetMixin.java
@@ -0,0 +1,26 @@
+package io.github.potassiummc.thorium.mixin.client;
+
+import net.minecraft.client.gui.widget.SliderWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SliderWidget.class)
+public class SliderWidgetMixin {
+
+    private boolean wasOpened = false;
+
+    // Fix MC-145748
+    @Inject(method = "onClick(DD)V", at = @At("TAIL"))
+    public void onClickSetWasOpened(double mouseX, double mouseY, CallbackInfo ci) {
+        wasOpened = true;
+    }
+
+    @Inject(method = "onRelease(DD)V", at = @At("HEAD"), cancellable = true)
+    public void onReleaseCancelIfNotOpened(double mouseX, double mouseY, CallbackInfo ci) {
+        if (wasOpened) return;
+        ci.cancel();
+    }
+
+}
