14f94b54aa5e704738d1423d0ca991bee0f34507
Noah van der Aa <ndvdaa@gmail.com>
2022-06-12 22:27:55 +0200

    Fix end terrain not generating at all

--- src/main/java/io/github/potassiummc/thorium/mixin/server/mc159283/EndIslandsMixin.java
+++ src/main/java/io/github/potassiummc/thorium/mixin/server/mc159283/EndIslandsMixin.java
@@ -5,11 +5,12 @@ import net.minecraft.util.math.noise.SimplexNoiseSampler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(targets = "net.minecraft.world.gen.densityfunction.DensityFunctionTypes$EndIslands")
 public class EndIslandsMixin {
 
-    @Redirect(method = "method_41529(Lnet/minecraft/util/math/noise/SimplexNoiseSampler;II)F", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;sqrt(F)F"))
+    @Redirect(method = "method_41529(Lnet/minecraft/util/math/noise/SimplexNoiseSampler;II)F", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;sqrt(F)F"), slice = @Slice(to = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;clamp(FFF)F")))
     private static float getNoiseAt(float value, SimplexNoiseSampler simplexNoiseSampler, int i, int j) {
         // Explicitly cast i and j to longs.
         return MathHelper.sqrt((long) i * (long) i + (long) j * (long) j);
