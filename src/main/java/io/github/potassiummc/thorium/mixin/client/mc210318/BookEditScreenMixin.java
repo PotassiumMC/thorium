dde7386c24fc983002010a71f5772630e2eff5c8
Noah van der Aa <ndvdaa@gmail.com>
2022-06-27 20:43:12 +0200

    Simplify fix for MC-210318 (closes #11)

--- src/main/java/io/github/potassiummc/thorium/mixin/client/mc210318/BookEditScreenMixin.java
+++ src/main/java/io/github/potassiummc/thorium/mixin/client/mc210318/BookEditScreenMixin.java
@@ -1,38 +1,17 @@
 package io.github.potassiummc.thorium.mixin.client.mc210318;
 
 import net.minecraft.client.gui.screen.ingame.BookEditScreen;
-import net.minecraft.client.util.SelectionManager;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Hand;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(BookEditScreen.class)
 public abstract class BookEditScreenMixin {
 
-    @Final
-    @Mutable
-    @Shadow
-    private SelectionManager bookTitleSelectionManager;
-
-    @Shadow
-    private String title;
-
-    @Shadow
-    protected abstract String getClipboard();
-
-    @Shadow
-    protected abstract void setClipboard(String clipboard);
-
-    @Inject(method = "<init>", at = @At(value = "TAIL"))
-    private void modifyBookTitleSelectionManager(PlayerEntity player, ItemStack itemStack, Hand hand, CallbackInfo ci) {
-        this.bookTitleSelectionManager = new SelectionManager(() -> this.title, (title) -> this.title = title, this::getClipboard, this::setClipboard, (string) -> string.length() <= 16);
+    @ModifyConstant(method = "method_27593(Ljava/lang/String;)Z", constant = @Constant(intValue = 16), allow = 1)
+    private static int getCorrectMaxLength(int constant) {
+        // <= 16 === < 17
+        return 17;
     }
 
 }
